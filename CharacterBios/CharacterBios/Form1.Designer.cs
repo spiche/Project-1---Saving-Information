51cc33c46377631150920ff0e0c2942fee163609
Shelby Piche <shelby.piche@maine.edu>
2012-02-15 11:39:21 -0500

    my first command

---
+++ CharacterBios/CharacterBios/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace CharacterBios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.tbGenInfo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.database = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.database)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(12, 12);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 0;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Location = new System.Drawing.Point(12, 73);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(99, 20);
+            this.tbLastName.TabIndex = 1;
+            this.tbLastName.TextChanged += new System.EventHandler(this.tbLastName_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Last Name";
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.Location = new System.Drawing.Point(117, 73);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(99, 20);
+            this.tbFirstName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(114, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "First Name";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(94, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // tbGenInfo
+            // 
+            this.tbGenInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbGenInfo.Location = new System.Drawing.Point(12, 124);
+            this.tbGenInfo.Multiline = true;
+            this.tbGenInfo.Name = "tbGenInfo";
+            this.tbGenInfo.Size = new System.Drawing.Size(632, 198);
+            this.tbGenInfo.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "General Information";
+            // 
+            // database
+            // 
+            this.database.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.database.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader;
+            this.database.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.database.DefaultCellStyle = dataGridViewCellStyle1;
+            this.database.Location = new System.Drawing.Point(268, 13);
+            this.database.Name = "database";
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.database.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            this.database.Size = new System.Drawing.Size(376, 108);
+            this.database.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 334);
+            this.Controls.Add(this.database);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbGenInfo);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbFirstName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbLastName);
+            this.Controls.Add(this.btnNew);
+            this.Name = "Form1";
+            this.Text = "Character Database";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.database)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbFirstName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox tbGenInfo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView database;
+    }
+}
+
---
+++ CharacterBios/CharacterBios/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace CharacterBios
+{
+    public partial class Form1 : Form
+    {
+        private string lastN = null;
+        private DataTable table = new DataTable();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //Save button
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+         
+            //saves information to a file by last name
+            if (lastN != null)
+            {
+                if (tbGenInfo.Text.Length > 30 )
+                {
+                    table.Rows.Add(tbLastName.Text, tbFirstName.Text, tbGenInfo.Text.Substring(0, 30));
+                }
+                else
+                {
+                    table.Rows.Add(tbLastName.Text, tbFirstName.Text, tbGenInfo.Text);
+                }
+                TextWriter tw = new StreamWriter(lastN + ".txt");
+
+                tw.WriteLine(tbLastName.Text);
+                tw.WriteLine(tbFirstName.Text);
+                tw.Write(tbGenInfo.Text);
+
+                tw.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a Last Name");
+            }
+
+            lastN = null;
+
+      
+        }
+
+        //New Button
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            lastN = null;
+            tbGenInfo.Clear();
+            tbFirstName.Clear();
+            tbLastName.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            table.Columns.Add("Last");
+            table.Columns.Add("First");
+            table.Columns.Add("Other");
+            database.DataSource = table;
+        }
+
+        private void tbLastName_TextChanged(object sender, EventArgs e)
+        {
+            if (tbLastName.Text != "")
+            {
+                lastN = tbLastName.Text;
+            }
+        }
+
+
+    }
+
+
+}
